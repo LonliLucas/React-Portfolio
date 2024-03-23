f391b6ba634a16632d4d070e8558d7a53edfbbc3
LonliLucas <lukej.gilless@gmail.com>
2024-03-23 11:58:21 -0600

    Added base file structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+
+export default function App() {
+    return (
+        <>
+            <Header />
+            <Sidebar />
+        </>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <div className="headerClass">
+            <header>
+                <h1>Lucas Gilles</h1>
+            </header>
+        </div>
+    )
+}
