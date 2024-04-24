2e9f0842c0d785a562fedb449003916f90da1823
LonliLucas <lukej.gilless@gmail.com>
2024-04-24 15:59:17 -0600

    Changed color palette and added some to the contact me section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,9 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 
 export default function About() {
     return (
         <div className="center-flex about" id="aboutMe">
-            <image></image>
             <a>About me section</a>
         </div>
     )
--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,9 +1,11 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 
 export default function ContactMe() {
     return (
-        <div id="contactMe">
-
+        <div className="contact-me"  id="contactMe">
+            <a>Email me at:</a>
+            <a>lukej.gilless@gmail.com</a>
         </div>
     )
 }
